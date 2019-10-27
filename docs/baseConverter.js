f8143b714c1b1c700041f74cb1cd2aed43108473
GwenaelleH <56806151+GwenaelleH@users.noreply.github.com>
2019-10-27 21:16:37 +0100

    Source code
    Feel free to modify but keep it open source !

---
+++ docs/baseConverter.js
@@ -0,0 +1,58 @@
+var characterList = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '+', '/'];
+
+function baseToDec(number, base) {      
+    var number = number + '';
+    var result = 0;
+    var base = parseInt(base);
+
+    for (var i = 0; i < number.length; i++) {   
+        if (characterList.includes(number[number.length-1-i])) {
+        	result += (10 + characterList.indexOf(number[number.length-1-i]))*Math.pow(base, i);
+        }
+        else {
+        	result += parseInt(number[number.length-1-i])*Math.pow(base, i);
+        }
+        
+    }
+    
+    return result;      
+} 
+
+function decToBase(number, base) {
+	var base = parseInt(base);
+	var number = parseInt(number);
+	var result = '';
+
+	while (Math.floor(number/base) != 0) {
+		if (number%base > 9) {
+			result = characterList[(number%base)-10] + result;
+		}
+		else {
+			result = number%base + result;
+		}
+		number = Math.floor(number/base);
+	}
+	if (number%base > 9) {
+			result = characterList[(number%base)-10] + result;
+		}
+	else {
+		result = number%base + result;
+	}
+	return result;
+}
+
+
+function convert() {
+	var number = document.getElementById('number').value;
+	var fromBase = parseInt(document.getElementById('fromBase').value);
+	console.log(typeof fromBase);
+	console.log(!fromBase);
+	var toBase = parseInt(document.getElementById('toBase').value);
+
+	if (fromBase>=2 && fromBase <= 64 && toBase >= 2 && toBase <= 64) {
+		document.getElementById('converted').innerHTML = number + " in base " + fromBase + " gives " + decToBase(baseToDec(number, fromBase), toBase) + " in base " + toBase;
+	}
+	else {
+		document.getElementById('converted').innerHTML = "Bases can only be numbers between 2 and 64";
+	}
+}
