660e963e3482ecf474d857e6e8326584af7df64c
GwenaelleH <56806151+GwenaelleH@users.noreply.github.com>
2019-10-28 21:36:23 +0100

    Idiot Proof
    You can't put invalid characters or numbers anymore (at least not in a way that I know)

--- docs/baseConverter.js
+++ docs/baseConverter.js
@@ -1,4 +1,4 @@
-var characterList = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '+', '/'];
+var characterList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '+', '/'];
 
 function baseToDec(number, base) {      
     var number = number + '';
@@ -6,13 +6,7 @@ function baseToDec(number, base) {
     var base = parseInt(base);
 
     for (var i = 0; i < number.length; i++) {   
-        if (characterList.includes(number[number.length-1-i])) {
-        	result += (10 + characterList.indexOf(number[number.length-1-i]))*Math.pow(base, i);
-        }
-        else {
-        	result += parseInt(number[number.length-1-i])*Math.pow(base, i);
-        }
-        
+        result += (characterList.indexOf(number[number.length-1-i]))*Math.pow(base, i);        
     }
     
     return result;      
@@ -24,20 +18,13 @@ function decToBase(number, base) {
 	var result = '';
 
 	while (Math.floor(number/base) != 0) {
-		if (number%base > 9) {
-			result = characterList[(number%base)-10] + result;
-		}
-		else {
-			result = number%base + result;
-		}
+		result = characterList[(number%base)] + result;	
 		number = Math.floor(number/base);
 	}
-	if (number%base > 9) {
-			result = characterList[(number%base)-10] + result;
-		}
-	else {
-		result = number%base + result;
-	}
+	
+	result = characterList[(number%base)] + result;	
+	number = Math.floor(number/base);
+
 	return result;
 }
 
@@ -47,6 +34,12 @@ function convert() {
 	var fromBase = parseInt(document.getElementById('fromBase').value);
 	var toBase = parseInt(document.getElementById('toBase').value);
 
+	for (var i = 0; i<number.length; i++) {
+		if(characterList.indexOf(number[i]) >= parseInt(fromBase) || !characterList.includes(number[i])) {
+			document.getElementById('converted').innerHTML = "This is not a valid number for this base";
+			return;
+		}
+	}
 	if (fromBase>=2 && fromBase <= 64 && toBase >= 2 && toBase <= 64) {
 		document.getElementById('converted').innerHTML = number + " in base " + fromBase + " gives " + decToBase(baseToDec(number, fromBase), toBase) + " in base " + toBase;
 	}
